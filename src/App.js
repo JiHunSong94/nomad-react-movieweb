50de2350bee35124eb0363a1ebbf8d9cbcb9f767
Jihun Song <songjihun940409@gmail.com>
2022-06-28 17:58:50 +0900

    #7.5-React-Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
